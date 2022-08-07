05919f7ccc5b88e25af41c7b81d23cf98642add3
YadavGulshan <yadavgulshan542@gmail.com>
2022-08-08 00:58:36 +0530

    Added mantine and setted up theme provider for light and dark modes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,46 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import React from 'react';
+import {
+  ColorScheme,
+  ColorSchemeProvider,
+  MantineProvider,
+} from '@mantine/core';
+
+import { setCookie } from 'cookies-next';
 /**
- * @param {AppProps} Component The Component
+ * @param {AppProps} props The Component and Page props.
  * @return {JSX.Element}
  */
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+function MyApp(props: AppProps & { colorScheme: ColorScheme }): JSX.Element {
+  const { Component, pageProps } = props;
+  const [colorScheme, setColorScheme] = React.useState<ColorScheme>(
+    props.colorScheme,
+  );
+
+  const toogleColorScheme = (value?: ColorScheme) => {
+    const nextColorScheme =
+      value || (colorScheme === 'dark' ? 'light' : 'dark');
+    setColorScheme(nextColorScheme);
+    setCookie('mantine-color-scheme', nextColorScheme, {
+      maxAge: 30 * 24 * 60 * 60,
+    });
+  };
+
+  return (
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toogleColorScheme}
+    >
+      <MantineProvider
+        theme={{ colorScheme }}
+        withGlobalStyles
+        withNormalizeCSS
+      >
+        <Component {...pageProps} />;
+      </MantineProvider>
+    </ColorSchemeProvider>
+  );
 }
 
 export default MyApp;
